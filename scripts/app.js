70deedb07e046288042f40923eb9b48704710259
Alex Munoz <alex@munoz.us>
2020-05-23 12:15:37 -0400

    barebones website up and running

--- scripts/app.js
+++ scripts/app.js
@@ -32,3 +32,21 @@ app.use(bodyParser.json());
 app.get('/',function(req,res){
     res.render('home');
 });
+
+// Error Handling
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+// Start Server
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
